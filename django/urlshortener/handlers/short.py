d972899e5b84bf6e81a6cf746a660c616beb34dc
gmoryes <gmoryes@gmail.com>
2019-12-14 15:10:17 +0300

    [django] add test redirect to google

--- django/urlshortener/handlers/short.py
+++ django/urlshortener/handlers/short.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.shortcuts import redirect
+
 def index(request):
     return render(request, "index.html")
+
+def redirect_to_google(request):
+    return redirect('https://google.com')
