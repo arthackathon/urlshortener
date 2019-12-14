f60f30b7526c152cd587c2f84301f151fe407aa2
gmoryes <gmoryes@gmail.com>
2019-12-14 13:29:25 +0300

    add index.html

--- django/urlshortener/urls.py
+++ django/urlshortener/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from handlers import short
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('short/', short.index, name='index'),
 ]
